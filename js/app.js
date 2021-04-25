d28c6ab689c50737b0e9271d8a605dc230efb073
mehmettunc55 <mtunc551980@gmail.com>
2021-04-25 03:07:19 +0300

    body image added

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const pictures = [
 
 ];
 
+const bodyImg = document.querySelector('body');
 const buttons = document.querySelectorAll('.btn');
 const imgContainer = document.querySelector('.img-container');
 
@@ -20,6 +21,7 @@ buttons.forEach(function(button){
         counter = pictures.length-1
       }
       imgContainer.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
+      bodyImg.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
     }
     if(button.classList.contains('btn-right')){
       counter++
@@ -27,41 +29,7 @@ buttons.forEach(function(button){
         counter = 0
       }
       imgContainer.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
+      bodyImg.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
     }
   })
 })
-
-
-/* 
-(function() {
-  const pictures = [
-    "contBcg-0",
-    "contBcg-1",
-    "contBcg-2",
-    "contBcg-3",
-    "contBcg-4"
-  ];
-
-  const buttons = document.querySelectorAll('.btn')
-  const imageDIV = document.querySelector('.img-container')
-  let counter = 0
-
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-      if (button.classList.contains('btn-left')){
-        counter--
-        if(counter < 0){
-          counter = pictures.length -1
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-      if (button.classList.contains('btn-right')){
-        counter++
-        if(counter > pictures.length -1){
-          counter = 0
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-    })
-  })
-})(); */
